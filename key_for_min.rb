342925430bbad5a2295e39358f1e3667274bdbaf
Benjamin Wichser <benwichser@gmail.com>
2020-03-26 21:35:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   first_loop = true 
-  lowest_key = nil 
-  lowest_value = 0 
+  #lowest_key = nil 
+  #lowest_value = 0 
   name_hash.each do |name, value|
     if first_loop
       first_loop = false  
